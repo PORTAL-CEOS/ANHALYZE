1a37fb3990abd6d67b9045ef241b0082656776b1
Emilio Enriquez <jeenriquez@gmail.com>
2023-02-07 18:08:40 -0600

    Adding all current files Im testing with.

---
+++ anha4_utils.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+#data related libraries
+import numpy as np
+import netCDF4 as nc
+
+
+#plotting related libraries
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set()
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+
+
+
+
