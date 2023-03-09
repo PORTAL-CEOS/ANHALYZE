2c70e6d58bf6772fd32980e64f90bcf61a3b4197
Emilio Enriquez <jeenriquez@gmail.com>
2023-03-09 17:47:13 -0600

    Adding plot function, this includes Cartopy projection features.

--- scripts/anha4_utils.py
+++ scripts/anha4_utils.py
@@ -6,6 +6,11 @@ import numpy as np
 import netCDF4 as nc
 
 # Plotting-related libraries
+import matplotlib
+import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1.inset_locator import inset_axes
+import cartopy
+import cartopy.crs as ccrs
 
 # OS-specific libraries
 from sys import platform
@@ -113,3 +118,56 @@ def get_row_col_range(data, lat_range, lon_range):
     col_range = (col_ranges[0], col_ranges[-1])
 
     return row_range, col_range
+
+
+def plot_var_data(data, lat_range, lon_range, depth=0, var='votemper'):
+    """  """
+
+    # Get var data
+    var_data = get_var_data(data, lat_range, lon_range, depth=depth)
+
+    # Get mask
+    surf_mask = get_mask(data, lat_range, lon_range, depth=depth)
+
+    # Mask data
+    var_data.data[~np.ma.filled((1 == surf_mask.data))] = np.nan
+    surf_mask.data[np.ma.filled((1 == surf_mask.data))] = np.nan
+
+    # getting lat and lon
+    lat, lon = get_lat_lon(data, lat_range, lon_range)
+
+    # Setting plotting vars.
+    levels = 42
+    vmax = 20.
+    vmin = -20.
+    standard_parallels = (55, 60)
+    central_longitude = -80
+    cmap = 'coolwarm'
+    ocean_color = '#000066'
+    land_color = matplotlib.colors.to_hex('wheat')
+    
+    # Set up plot
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1,
+                         projection=ccrs.LambertConformal(central_longitude=central_longitude,
+                                                          standard_parallels=standard_parallels))
+    ax.set_extent([lon_range[0], lon_range[1], lat_range[0], lat_range[1]])
+
+    # Adding ocean and land features
+    ax.add_feature(cartopy.feature.OCEAN, facecolor=ocean_color)
+    ax.add_feature(cartopy.feature.LAND, facecolor=land_color)
+
+    # Plotting var data as filled countour regions
+    im = ax.contourf(lon, lat, var_data, levels=levels, cmap=cmap,
+                     vmin=vmin, vmax=vmax, transform=ccrs.PlateCarree())
+
+    # Create grid-line labels
+    gl = ax.gridlines(crs=ccrs.PlateCarree(), draw_labels=True, x_inline=False,
+                      y_inline=False, color='k', alpha=.3, linewidths=.01)
+    gl.right_labels = gl.top_labels = False
+
+    # Set Color-bar
+    axins = inset_axes(ax, width="5%", height="100%", loc='right', borderpad=-1)
+    label = data.variables[var].long_name + ' [' + data.variables[var].units + ']'
+    fig.colorbar(im, cax=axins, orientation="vertical", label=label, format='%.1f')
+
