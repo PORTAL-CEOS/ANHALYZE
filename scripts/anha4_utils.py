b4aada310535fab74ea8dec1c3bf7c370c6b303a
Emilio Enriquez <jeenriquez@gmail.com>
2023-03-20 20:32:44 -0500

    Adding a function to show multiple maps given a list of files. Adding a function for setting up either Hudson Bay or James Bay, needs more development.

--- scripts/anha4_utils.py
+++ scripts/anha4_utils.py
@@ -239,6 +239,113 @@ def show_var_data_map(data, lat_range, lon_range, depth=0, var='votemper'):
     fig.colorbar(im, cax=axins, orientation="vertical", label=label, format='%.1f')
 
 
+def init_location(Hudson_bay=True):
+    """
+    Setting up model parameters based on Hudson Bay and James Bay locations.
+
+    Hudson_bay : Boolean if using Hudson Bay vs James Bay locations.
+    """
+
+    if Hudson_bay:
+        proj_size = 2
+        region = 'HudsonBay'
+    else:
+        proj_size = 4
+        region = 'JamesBay'
+
+    return region, proj_size
+
+
+def show_var_data_maps(file_list, lat_range, lon_range, depth=0, var='votemper'):
+    """ Displays map of given var in lat-lon range and depth.
+        Note: depth has not been tested.
+    """
+
+    # Setting plotting vars.
+    levels = 42
+    vmax = 20.
+    vmin = -20.
+    standard_parallels = (52.5, 62.5)
+    central_longitude = -80
+    cmap = 'coolwarm'
+
+    region, proj_size = init_location()
+
+    # Get
+    date_start = get_date(file_list[0])
+    date_end = get_date(file_list[-1])
+
+    # Setting up figure
+    f_size = 4
+    ncols = 3
+    nrows = int(np.ceil(len(file_list) / ncols))
+    fig, ax = plt.subplots(ncols=ncols, nrows=nrows, figsize=[ncols * f_size, nrows * f_size + 1],
+                           subplot_kw={'projection': ccrs.LambertConformal(central_longitude=central_longitude,
+                                                                           standard_parallels=standard_parallels)})
+
+    # Looping over files
+    for i, xx in enumerate(fig.axes):
+
+        # Stop loop when number of subplots is larger than the number of files.
+        if i == len(file_list):
+            break
+
+        # Get data
+        data = nc.Dataset(file_list[i])
+
+        # Get var data
+        var_data = get_var_data(data, lat_range, lon_range, depth=depth, var=var)
+
+        # getting lat and lon
+        if i == 0:
+            lat, lon = get_lat_lon(data, lat_range, lon_range)
+
+        # Plotting data
+        # Set map extent
+        xx.set_extent([lon_range[0], lon_range[1], lat_range[0], lat_range[1]])
+
+        # Set map features
+        xx.add_feature(get_feature_mask())
+        xx.add_feature(get_feature_mask(feature='ocean'))
+
+        # Plotting var data as filled contour regions
+        im = xx.contourf(lon, lat, var_data, levels=levels, cmap=cmap,
+                         vmin=vmin, vmax=vmax, transform=ccrs.PlateCarree(), zorder=2)
+
+        # Plotting data contour lines
+        xx.contour(lon, lat, var_data, levels=levels, cmap='Greys', linewidths=.2, transform=ccrs.PlateCarree())
+
+        # Create grid-line labels
+        gl = xx.gridlines(crs=ccrs.PlateCarree(), draw_labels=True, x_inline=False,
+                          y_inline=False, color='k', alpha=.3, linewidths=.01)
+        gl.right_labels = gl.top_labels = False
+
+        # Set axis labels
+        xx.text(0.5, -0.2, 'Longitude', va='bottom', ha='center',
+                rotation='horizontal', rotation_mode='anchor',
+                transform=xx.transAxes)
+        if i % ncols == 0:
+            xx.text(-0.25, 0.55, 'Latitude', va='bottom', ha='center',
+                    rotation='vertical', rotation_mode='anchor',
+                    transform=xx.transAxes)
+
+        # Set title
+        xx.set_title(file_list[i].split('/')[-1].split('_')[1])
+
+        # Set Color-bar
+        axins = inset_axes(xx, width="5%", height="100%", loc='right', borderpad=-1)
+        label = '%s [%s]' % (data.variables[var].long_name.title(), data.variables[var].units.title())
+        fig.colorbar(im, cax=axins, orientation="vertical", label=label, format='%.1f')
+
+    fig.suptitle(data.variables[var].standard_name.replace('_', ' '), fontsize=16)
+    fig.subplots_adjust(bottom=0.1, top=0.9, left=0.07, right=.92, wspace=.65, hspace=.1)
+
+    # Save figure to file.
+    if platform == "linux" or platform == "linux2":
+        output_fig_name = '../figs/%s_%s_%s-%s.png' % (region, data.variables[var].long_name, date_start, date_end)
+        plt.savefig(output_fig_name, bbox_inches='tight', dpi=500)
+
+
 def calc_stats_var_data(data, lat_range, lon_range, depth=0, no_min_max=True, var='votemper'):
     """  """
 
