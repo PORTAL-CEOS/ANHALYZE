b8e053ddb4b2be0fb57989a3277faee255d2edc3
Emilio Enriquez <jeenriquez@gmail.com>
2024-09-10 18:04:56 -0500

    [download_mask] Updating link that works and make it global variable.

--- anhalyze/core/download_mask.py
+++ anhalyze/core/download_mask.py
@@ -3,6 +3,8 @@
 
 import requests
 
+MASK_URL = 'https://bit.ly/3TkYlGL'
+
 
 def download_large_file(url, destination):
     """ Download a large file given url into destination
@@ -46,9 +48,7 @@ def test_download():
 if __name__ == '__main__':
 
     # URL location for mask.
-    url = 'https://drive.google.com/file/d/17f6Lb0qy_mB5zSw2ZVEZt8PiqcnFu-S7/view?usp=sharing'
-    # url2 = 'https://umanitoba-my.sharepoint.com/personal/dasilvlh_myumanitoba_ca/_layouts/15/onedrive.aspx?id=%2Fpersonal%2Fdasilvlh%5Fmyumanitoba%5Fca%2FDocuments%2FANHALIZE%2FRESOURCES%2FANHA4%5Fmask%2Enc&parent=%2Fpersonal%2Fdasilvlh%5Fmyumanitoba%5Fca%2FDocuments%2FANHALIZE%2FRESOURCES&ga=1'
-    # url2 = 'https://umanitoba-my.sharepoint.com/personal/dasilvlh_myumanitoba_ca/_layouts/15/download.aspx?UniqueId=a07a5b33-7508-41a7-ad89-d63e84169096'
+    url = MASK_URL
 
     # Filename
     destination = 'ANHA4_mask_test.nc'
